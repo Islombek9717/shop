25b5c314271b4ee0fae73d9872b99946458d07ec
Islombek <sultonovislom9717@gmail.com>
2022-02-15 01:26:40 +0500

    add and finish slider component

---
+++ src/data.js
@@ -0,0 +1,27 @@
+import Slider1 from "./assets/images/background1.png";
+import Slider2 from "./assets/images/background2.png";
+import Slider3 from "./assets/images/background3.jpg";
+
+export const sliderItems = [
+  {
+    id: 1,
+    img: `${Slider1}`,
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: `${Slider2}`,
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: `${Slider3}`,
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
