b71ba66a0edc6ab6fab7d665f5ddb965b634b3af
Islombek <sultonovislom9717@gmail.com>
2022-02-24 15:44:43 +0500

    get data from api

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { popularProducts } from "../data";
 import Product from "./Product";
+import axios from "axios";
 
 const Container = styled.div`
   padding: 20px;
@@ -14,7 +15,23 @@ const Products = ({ cat, filters, sort }) => {
   const [products, SetProducts] = useState([]);
   const [filteredProducts, SetFilteredProducts] = useState([]);
 
-  useEffect(() => {}, [cat]);
+  const options = {
+    method: "GET",
+    url: "https://fashion4.p.rapidapi.com/v1/version",
+    headers: {
+      "x-rapidapi-host": "fashion4.p.rapidapi.com",
+      "x-rapidapi-key": "26160fa9c2msh714175ba676e9c6p18c490jsn10abdba15b39",
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
 
   return (
     <Container>
